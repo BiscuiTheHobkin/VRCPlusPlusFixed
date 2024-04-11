62f191d18e603d12f6bc162935af015e12f19bb6
Biscuit <84092899+BiscuiTheHobkin@users.noreply.github.com>
2024-04-11 09:49:01 +0200

    Code Upload

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using Il2CppVRC.SDKBase;
+using MelonLoader;
+using System.IO;
+using UnityEngine;
+
+namespace VRCPlusPlusFixed
+{
+    public static class BuildInfo
+    {
+        public const string Name = "VRCPlusPlus";
+        public const string Description = "Get some worlds extras perks";
+        public const string Author = "FS21114 | Fixed by Biscuit";
+        public const string Company = null;
+        public const string Version = "1.0";
+        public const string DownloadLink = null;
+    }
+
+    public class VRCPlsPlus : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+        MelonLogger.Msg("Starting VRCPlusPlus...");
+        }
+        public override void OnLateUpdate()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.P))
+            {
+                Networking.LocalPlayer.displayName = File.ReadAllText("UserData/VRCPlusPlus.cfg");
+            }
+        }
+    }
+ }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(VRCPlusPlusFixed.BuildInfo.Description)]
+[assembly: AssemblyDescription(VRCPlusPlusFixed.BuildInfo.Description)]
+[assembly: AssemblyCompany(VRCPlusPlusFixed.BuildInfo.Company)]
+[assembly: AssemblyProduct(VRCPlusPlusFixed.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + VRCPlusPlusFixed.BuildInfo.Author)]
+[assembly: AssemblyTrademark(VRCPlusPlusFixed.BuildInfo.Company)]
+[assembly: AssemblyVersion(VRCPlusPlusFixed.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(VRCPlusPlusFixed.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(VRCPlusPlusFixed.VRCPlsPlus), VRCPlusPlusFixed.BuildInfo.Name, VRCPlusPlusFixed.BuildInfo.Version, VRCPlusPlusFixed.BuildInfo.Author, VRCPlusPlusFixed.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
